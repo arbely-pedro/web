26175a0f80b624ca9a043fe9d1b93a61cdfe4a9d
arbely-pedro <proyectocobayfisio@outlook.com>
2025-03-27 03:07:46 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function subirPagina() {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-}
-
-document.querySelector('.btn-enviar').addEventListener('click', function(event) {
-    event.preventDefault(); // Evita el envío del formulario
-    alert('Solicitud enviada'); // Muestra la alerta
-});
